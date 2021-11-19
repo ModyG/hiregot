9c3361876a090a7596d2ffadda7f2cb52895322c
Mody <jaguar_m24@hotmail.com>
2021-11-19 17:09:46 +0000

    make it better

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -40,13 +40,15 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <TotalContainer>TOTAL: ${total}</TotalContainer>
+    <TotalContainer>TOTAL: £{total}</TotalContainer>
+    
+    <StripeCheckoutButton price={total} />
+
     <WarningContainer>
       *Please use the following test credit card for payments*
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
     </WarningContainer>
-    <StripeCheckoutButton price={total} />
   </CheckoutPageContainer>
 );
 
